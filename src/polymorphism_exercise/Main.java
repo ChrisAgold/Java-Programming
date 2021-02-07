3816823618bce73d1d7057d88a35431688711886
Chris Agold <christophersagold@gmail.com>
2021-02-06 20:23:28 -0600

    created car object with method overide

--- src/polymorphism_exercise/Main.java
+++ src/polymorphism_exercise/Main.java
@@ -1,22 +1,83 @@
 package polymorphism_exercise;
 
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+    // Constructor
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    // Getter
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Methods
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
+// Make new car class
+class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+    // Override parent methods
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
 public class Main {
-    // We are going to go back to the car analogy.
-    // Create a base class called Car
-    // It should have a few fields that would be appropriate for a generice car calss.
-    // engine, cylinders, wheels, etc.
-    // Constructor should initialize cylinders (number of) and name, and set wheels to 4
-    // and engine to true. Cylinders and names would be passed parameters.
-    //
-    // Create appropriate getters
-    //
-    // Create some methods like startEngine, accelerate, and brake
-    //
-    // show a message for each in the base class
-    // Now create 3 sub classes for your favorite vehicles.
-    // Override the appropriate methods to demonstrate polymorphism in use.
-    // put all classes in the one java file (this one).
-
-
-
+    public static void main(String[] args) {
+        // We are going to go back to the car analogy.
+        // Create a base class called Car
+        // It should have a few fields that would be appropriate for a generice car calss.
+        // engine, cylinders, wheels, etc.
+        // Constructor should initialize cylinders (number of) and name, and set wheels to 4
+        // and engine to true. Cylinders and names would be passed parameters.
+        //
+        // Create appropriate getters
+        //
+        // Create some methods like startEngine, accelerate, and brake
+        //
+        // show a message for each in the base class
+        // Now create 3 sub classes for your favorite vehicles.
+        // Override the appropriate methods to demonstrate polymorphism in use.
+        // put all classes in the one java file (this one).
+        Car car = new Car(8, "Base Car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+    }
 }
